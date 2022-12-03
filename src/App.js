259b0b024c1d79e63a4d3ddf80c8ad319f99d34b
Odmaral Tumenjargal <odmaraltumenjargal@Odmarals-MacBook-Air.local>
2022-12-03 13:49:23 -0800

    added err msg

--- src/App.js
+++ src/App.js
@@ -37,39 +37,42 @@ function App() {
 
   return (
     <div>
-      <div id="full" className="App">
-        {" "}
-        <div>
-          <div id="navBar">
-            <p id="title">To-do List</p>{" "}
-          </div>
-          <div style={{ overflow: "scroll" }} id="new">
-            <TodoForm addTask={addTask} />
+      <h1 id="errormsg">This site can only be viewed on desktop</h1>
+      <div id="fullPage">
+        <div id="full" className="App">
+          {" "}
+          <div>
+            <div id="navBar">
+              <p id="title">To-do List</p>{" "}
+            </div>
+            <div style={{ overflow: "scroll" }} id="new">
+              <TodoForm addTask={addTask} />
 
-            <img
-              alt=""
-              id="trashIcon"
-              onClick={handleFilter}
-              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAABmJLR0QA/wD/AP+gvaeTAAAAY0lEQVRIiWNgGAWDGBxnYGD4j4aPUcvww1gMJxcfxmYBE7Vcigf8p4MdQwAw4pGjVhBhtYMecYwVjFo8avGoxaMWj1o8avHQsvgJFcx/TI4mT6hGcht5jxgYGDwodPgoGMIAAIf9LS9I+wcVAAAAAElFTkSuQmCC"
-            ></img>
+              <img
+                alt=""
+                id="trashIcon"
+                onClick={handleFilter}
+                src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAABmJLR0QA/wD/AP+gvaeTAAAAY0lEQVRIiWNgGAWDGBxnYGD4j4aPUcvww1gMJxcfxmYBE7Vcigf8p4MdQwAw4pGjVhBhtYMecYwVjFo8avGoxaMWj1o8avHQsvgJFcx/TI4mT6hGcht5jxgYGDwodPgoGMIAAIf9LS9I+wcVAAAAAElFTkSuQmCC"
+              ></img>
+            </div>
+            <p id="tasks">Tasks:</p>
+            <ToDoList
+              toDoList={toDoList}
+              handleToggle={handleToggle}
+              handleFilter={handleFilter}
+            />
           </div>
-          <p id="tasks">Tasks:</p>
-          <ToDoList
-            toDoList={toDoList}
-            handleToggle={handleToggle}
-            handleFilter={handleFilter}
-          />
         </div>
-      </div>
-      <div id="deleted">
-        <p id="deletedTasks">Deleted Tasks</p>
-        {deleteTasks.map((task) => {
-          return (
-            <p style={{ paddingTop: "10px", paddingLeft: "20px" }}>
-              {task.task}
-            </p>
-          );
-        })}
+        <div id="deleted">
+          <p id="deletedTasks">Deleted Tasks</p>
+          {deleteTasks.map((task) => {
+            return (
+              <p style={{ paddingTop: "10px", paddingLeft: "20px" }}>
+                {task.task}
+              </p>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
